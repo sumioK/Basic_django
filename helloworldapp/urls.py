7113fbdbd72c0bebf93ec58b18e86adb42bacd99
sumiok <tap.miwo@gmail.com>
2023-02-21 19:30:05 +0900

    new app

---
+++ helloworldapp/urls.py
@@ -0,0 +1,5 @@
+from django.urls import path
+from views import helloworldappview
+urlpatterns = [
+    path("helloworldapp/", helloworldappview),
+]
--- helloworldproject/urls.py
+++ helloworldproject/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from .views import helloworldfunction, HelloworldClass
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('helloworld/', helloworldfunction),
     path('helloworld2/', HelloworldClass.as_view()),
+    path('', include('helloworldapp.urls'))
 ]
